83a6782a1fd886c763c9cca9726152a0238157c2
ChandrashekarOllala <82563584+chanduollala@users.noreply.github.com>
2022-01-29 22:00:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-
 import kivy
 from kivy.lang import Builder
 from kivymd.app import MDApp
@@ -10,42 +9,51 @@ from kivymd.uix.card import MDCard
 import requests
 from bs4 import BeautifulSoup
 from kivymd.uix.snackbar import Snackbar
-from openpyxl import *
 from datetime import datetime
+
 
 class HelloScreen(MDScreen):
     pass
 
+
 class HomePage(MDScreen):
     pass
+
+
 class PostLogin(MDScreen):
     pass
+
+
 class Weather(MDScreen):
     pass
+
 
 class Overview(MDScreen):
     pass
 
+
 class Transactions(MDScreen):
     pass
+
+
 class AddTransaction(MDScreen):
     pass
+
+
 class TCard(MDCard):
     pass
 
-kvstring='''<HelloScreen>:
 
+kvstring = '''<HelloScreen>:
     Image:
-        source:'logo.png'
+        source:'pic.png'
         size_hint_x: 1
         pos_hint: {"center_x": 0.5, "center_y": 0.5}
-
     MDRaisedButton:
         text: "Continue to APP"
         font_size: 20
         pos_hint: {"center_x": 0.5, "center_y": 0.15}
         on_press: app.continue_to_app()
-
 <HomePage>:
     MDLabel:
         text: "WELCOME TO TRIPOTO"
@@ -54,7 +62,6 @@ kvstring='''<HelloScreen>:
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
     MDLabel:
         text: "Enter details of Teammates"
         font_size: 25
@@ -69,7 +76,6 @@ kvstring='''<HelloScreen>:
         padding: 25
         spacing: 25
         orientation: 'vertical'
-
     MDTextField:
         id: phn1
         hint_text: "Person 1 Phone"
@@ -87,7 +93,6 @@ kvstring='''<HelloScreen>:
         helper_text: "Enter a name"
         font_size: 25
         pos_hint: {"center_x": 0.25, "center_y": 0.65}
-
     MDTextField:
         id: p2
         hint_text: "Person 2"
@@ -96,7 +101,6 @@ kvstring='''<HelloScreen>:
         pos_hint: {"center_x": 0.25, "center_y": 0.55}
         helper_text_mode: "on_error"
         helper_text: "Enter a name"
-
     MDTextField:
         id: phn2
         hint_text: "Person2 phone"
@@ -114,7 +118,6 @@ kvstring='''<HelloScreen>:
         pos_hint: {"center_x": 0.25, "center_y": 0.45}
         helper_text_mode: "on_error"
         helper_text: "enter a name"
-
     MDTextField:
         id: phn3
         hint_text: "Person 3 phone"
@@ -132,7 +135,6 @@ kvstring='''<HelloScreen>:
         pos_hint: {"center_x": 0.25, "center_y": 0.35}
         helper_text_mode: "on_error"
         helper_text: "Enter a name"
-
     MDTextField:
         id: phn4
         hint_text: "Person 4 phone"
@@ -142,7 +144,6 @@ kvstring='''<HelloScreen>:
         max_text_length: 10
         helper_text_mode: "on_error"
         helper_text: "enter 10 digit phone number"
-
     MDTextField:
         id: p5
         hint_text: "Person 5"
@@ -168,8 +169,6 @@ kvstring='''<HelloScreen>:
         required: True
         helper_text_mode: "on_error"
         helper_text: "Enter any place"
-
-
     MDRoundFlatButton:
         text: "Submit"
         font_size: 25
@@ -178,129 +177,100 @@ kvstring='''<HelloScreen>:
         on_press: app.submit(p1.text,p2.text,p3.text,p4.text,p5.text,phn1.text,phn2.text,phn3.text,phn4.text,phn5.text,destination.text)
 <PostLogin>:
     MDLabel:
-
         text: "Planning for a Trip this week?"
         font_size: 18
         pos_hint: {"center_x": 0.5, "center_y": 0.75}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
     MDRoundFlatButton:
-        text: "Check Weather condition at your destination"
+        text: "Check Weather condition at your destination"        
         font_size: 20
         pos_hint: {"center_x": 0.5, "center_y": 0.65}
         on_press: app.weather()
 
 
-    MDLabel:
-        text: "Already Registered?"
-        font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.55}
-        halign: 'center'
-        size_hint_y: None
-        padding_y: 15
-
-    MDRoundFlatButton:
-        text: "Know your location"
-        font_size: 20
-        pos_hint: {"center_x": 0.5, "center_y": 0.45}
-
 
     MDLabel:
         text: "Trip Started?"
         font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.35}
+        pos_hint: {"center_x": 0.5, "center_y": 0.45}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
     MDRoundFlatButton:
         text: "Track expenses"
         font_size: 20
-        pos_hint: {"center_x": 0.5, "center_y": 0.25}
+        pos_hint: {"center_x": 0.5, "center_y": 0.35}
         on_press: app.track()
+
 
 <Weather>:
     MDLabel:
-
-        text: "The weather details at "+app.destination+" is"
-        font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.85}
+        text: "The weather details at "+app.destination+" is: "
+        font_size: 30
+        pos_hint: {"center_x": 0.5, "center_y": 0.90}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
-
-
-
     MDLabel:
         text: 'Current Temperature:'+str(app.current_temperature)
-        font_size: 50
-        pos_hint: {"center_x": 0.5, "center_y": 0.75}
+        font_size: 25
+        pos_hint: {"center_x": 0.5, "center_y": 0.80}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
     MDLabel:
-
         text: 'Humidity:'+str(app.humidity)
-
+        font_size: 18
+        pos_hint: {"center_x": 0.5, "center_y": 0.70}
+        halign: 'center'
+        size_hint_y: None
+        padding_y: 15
+    MDLabel:
+        text: 'Minimum Temperature:'+str(app.tempmin)
         font_size: 18
         pos_hint: {"center_x": 0.5, "center_y": 0.65}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
     MDLabel:
-
-        text: 'Minimum Temperature:'+str(app.tempmin)
-
-        font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.55}
-        halign: 'center'
-        size_hint_y: None
-        padding_y: 15
-
-    MDLabel:
-
         text: 'Maximum Temperature:'+str(app.tempmax)
-
         font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.45}
+        pos_hint: {"center_x": 0.5, "center_y": 0.60}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
 
     MDLabel:
+        text: "The location details of "+app.destination+" is :"
+        font_size: 30
+        pos_hint: {"center_x": 0.5, "center_y": 0.50}
+        halign: 'center'
+        size_hint_y: None
+        padding_y: 15
 
+    MDLabel:
         text: 'Latitude:'+str(app.latitude)
-
+        font_size: 18
+        pos_hint: {"center_x": 0.5, "center_y": 0.40}
+        halign: 'center'
+        size_hint_y: None
+        padding_y: 15
+    
+    MDLabel:
+        text: 'Longitude:'+str(app.longitude)
         font_size: 18
         pos_hint: {"center_x": 0.5, "center_y": 0.35}
         halign: 'center'
         size_hint_y: None
         padding_y: 15
-
-    MDLabel:
-
-        text: 'Longitude:'+str(app.longitude)
-
-        font_size: 18
-        pos_hint: {"center_x": 0.5, "center_y": 0.25}
-        halign: 'center'
-        size_hint_y: None
-        padding_y: 15
-
-
-
-
 
     MDRoundFlatButton:
         text: "Go to Home Page"
         font_size: 20
         pos_hint: {"center_x": 0.5, "center_y": 0.15}
         on_press: app.goback()
-
 <Overview>:
     MDRaisedButton:
         text: "Overview"
@@ -311,15 +281,12 @@ kvstring='''<HelloScreen>:
         size_hint: 0.5,0.075
         on_press: app.overview()
         border: 'yellow'
-
     MDFlatButton:
         text: "Transactions"
         font_size: 18
         pos_hint: {"center_x": 0.75, "center_y": 0.85}
         on_press: app.transactions()
         size_hint: 0.5,0.075
-
-
     MDCard:
         size_hint: 0.48, 0.32
         pos_hint: {"center_x": 0.27, "center_y": 0.65}
@@ -370,7 +337,6 @@ kvstring='''<HelloScreen>:
             halign: left
             pos_hint: {"center_x": 0.5, "center_y": 0.5}
             halign: 'center'
-
         MDLabel:
             text: str(app.spends[app.persons[1]])
             font_size: 15
@@ -395,7 +361,6 @@ kvstring='''<HelloScreen>:
             halign: left
             pos_hint: {"center_x": 0.5, "center_y": 0.5}
             halign: 'center'
-
     MDCard:
         size_hint: 0.48, 0.4
         pos_hint: {"center_x": 0.27, "center_y": 0.25}
@@ -446,7 +411,6 @@ kvstring='''<HelloScreen>:
             halign: left
             pos_hint: {"center_x": 0.5, "center_y": 0.5}
             halign: 'center'
-
         MDLabel:
             text: str(app.cwisespends["Utilities"])
             font_size: 15
@@ -471,33 +435,22 @@ kvstring='''<HelloScreen>:
             halign: left
             pos_hint: {"center_x": 0.5, "center_y": 0.5}
             halign: 'center'
-
     MDFloatingActionButtonSpeedDial:
         data: app.data
         rotation_root_button: True
         callback: app.plus
-
-
     Widget:
         size_hint_y: None
         height: 10
     MDNavigationLayout:
-
         ScreenManager:
-
             MDScreen:
-
                 MDToolbar:
                     title: "Track Expenses"
                     elevation: 10
                     pos_hint: {"top": 1}
-
                     left_action_items:
                         [['keyboard-backspace', lambda x: app.goback()]]
-
-
-
-
 <Transactions>:
     MDRaisedButton:
         text: "Transactions"
@@ -508,7 +461,6 @@ kvstring='''<HelloScreen>:
         size_hint: 0.5,0.075
         on_press: app.transactions()
         border: 'yellow'
-
     MDFlatButton:
         text: "Overview"
         font_size: 18
@@ -521,41 +473,26 @@ kvstring='''<HelloScreen>:
         do_scroll_y: True
         size_hint: 1,0.80
         pos_hint: {"center_x": 0.5275, "y": 0}
-
         MDList:
-
             id: box
             spacing: 20
-
-
     MDFloatingActionButtonSpeedDial:
         data: app.data
         rotation_root_button: True
         callback: app.plus
-
     Widget:
         size_hint_y: None
         height: 10
     MDNavigationLayout:
-
         ScreenManager:
-
             MDScreen:
-
                 MDToolbar:
                     title: "Track Expenses"
                     elevation: 10
                     pos_hint: {"top": 1}
-
                     left_action_items:
                         [['keyboard-backspace', lambda x: app.goback()]]
-
-
-
-
-
 <AddTransaction>
-
     MDCard:
         size_hint: 0.95, 0.8
         pos_hint: {"center_x": 0.5, "center_y": 0.45}
@@ -573,8 +510,6 @@ kvstring='''<HelloScreen>:
         width: 180
         font_size: 25
         pos_hint: {"center_x": 0.50, "center_y": 0.75}
-
-
     MDTextField:
         id: purpose
         hint_text: "Purpose"
@@ -585,9 +520,6 @@ kvstring='''<HelloScreen>:
         required: True
         helper_text_mode: "on_error"
         helper_text: "Enter purpose"
-
-
-
     MDLabel:
         text: "Food"
         font_size: 20
@@ -611,7 +543,7 @@ kvstring='''<HelloScreen>:
         pos_hint: {'center_x': .8, 'center_y': .42}
         on_active: app.on_checkbox_active('Utilities')
     MDLabel:
-        text: "Tranvelling"
+        text: "Travelling"
         font_size: 20
         pos_hint: {"center_x": 0.7, "center_y": 0.35}
         size_hint_x: 0.3
@@ -643,8 +575,6 @@ kvstring='''<HelloScreen>:
         size: "48dp", "48dp"
         pos_hint: {'center_x': .8, 'center_y': .20}
         on_active: app.on_checkbox_active('Others')
-
-
     MDLabel:
         text: app.persons[0]
         font_size: 20
@@ -700,8 +630,6 @@ kvstring='''<HelloScreen>:
         size: "48dp", "48dp"
         pos_hint: {'center_x': .45, 'center_y': .20}
         on_active: app.on_person_select(app.persons[4])
-
-
     MDRaisedButton:
         text: "Add"
         font_size: 25
@@ -712,22 +640,14 @@ kvstring='''<HelloScreen>:
         size_hint_y: None
         height: 10
     MDNavigationLayout:
-
         ScreenManager:
-
             MDScreen:
-
                 MDToolbar:
                     title: "Add Transaction"
                     elevation: 10
                     pos_hint: {"top": 1}
-
                     left_action_items:
                         [['keyboard-backspace', lambda x: app.goback()]]
-
-
-
-
 <TCard>:
     id: tcard
     orientation: "vertical"
@@ -736,40 +656,31 @@ kvstring='''<HelloScreen>:
     focus_behavior: True
     ripple_behavior: True
     pos_hint: {"center_x": .5, "center_y": .5}
-
     MDBoxLayout:
         id: box_top
         spacing: "20dp"
         adaptive_height: True
-
-
-
         MDBoxLayout:
             id: text_box
             orientation: "vertical"
             adaptive_height: True
             spacing: "10dp"
             padding: 0, "10dp", "10dp", "10dp"
-
             MDLabel:
                 text: app.money
                 theme_text_color: "Primary"
                 font_style: "H5"
                 bold: True
                 adaptive_height: True
-
             MDLabel:
                 text: app.paidby+"    "+app.paymenttime
                 adaptive_height: True
                 theme_text_color: "Primary"
-
     MDSeparator:
-
     MDBoxLayout:
         id: box_bottom
         adaptive_height: True
         padding: "10dp", 0, 0, 0
-
         MDLabel:
             text: app.purpose
             adaptive_height: True
@@ -777,37 +688,36 @@ kvstring='''<HelloScreen>:
             theme_text_color: "Primary"
 '''
 
-
 Builder.load_string(kvstring)
 sm = ScreenManager()
 Window.size = (700, 600)
 
 
-class MainApp(MDApp):
+class TripotoApp(MDApp):
     def build(self):
         self.data = {
             'Add Transaction': 'Addtrs'
         }
 
         self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.primary_palette = "Orange"
         sm.add_widget(HelloScreen(name='helloscreen'))
         sm.add_widget(HomePage(name='homepage'))
 
         sm.current = 'helloscreen'
-        self.transactionsar=[]
+        self.transactionsar = []
         return sm
 
-
     def continue_to_app(self):
-        sm.current='homepage'
-    def submit(self,p1,p2,p3,p4,p5,phn1,phn2,phn3,phn4,phn5,des):
-        #if p1 and p2 and p3 and p4 and p5 and phn1 and phn2 and phn3 and phn4 and phn5 and des:
-        self.persons=[p1,p2,p3,p4,p5]
-        self.phones=[phn1,phn2,phn3,phn4,phn5]
-        self.destination=des
+        sm.current = 'homepage'
+
+    def submit(self, p1, p2, p3, p4, p5, phn1, phn2, phn3, phn4, phn5, des):
+        # if p1 and p2 and p3 and p4 and p5 and phn1 and phn2 and phn3 and phn4 and phn5 and des:
+        self.persons = [p1, p2, p3, p4, p5]
+        self.phones = [phn1, phn2, phn3, phn4, phn5]
+        self.destination = des
         sm.add_widget(PostLogin(name='postlogin'))
-        sm.current='postlogin'
+        sm.current = 'postlogin'
         sm.remove_widget(sm.get_screen('homepage'))
         '''else:
             Snackbar(
@@ -816,8 +726,9 @@ class MainApp(MDApp):
                 snackbar_y="10dp",
                 size_hint_x=.95
             ).open()'''
+
     def goback(self):
-        sm.current='postlogin'
+        sm.current = 'postlogin'
 
     def weather(self):
         api_request = requests.get("https://api.openweathermap.org/data/2.5/weather?q="
@@ -848,12 +759,12 @@ class MainApp(MDApp):
         s=BeautifulSoup(r.text,"html.parser")
         update=s.find("div",class_="BNeawe").text
         self.temp=update
-        
+
         '''
         sm.add_widget(Weather(name='weather'))
         sm.current = 'weather'
 
-    def addSubmit(self,amount,purpose):
+    def addSubmit(self, amount, purpose):
         row = [self.personname, amount, purpose, self.category, datetime.now().strftime("%D %H:%M")]
         self.transactionsar.append(row)
         Snackbar(
@@ -878,27 +789,26 @@ class MainApp(MDApp):
         except kivy.uix.screenmanager.ScreenManagerException:
             pass
 
-
-
-        c=['Food','Utilities','Travelling','Parties','Others']
-        self.cwisespends={"Food":0,"Utilities":0,"Travelling":0,"Parties":0,"Others":0}
-        self.spends={self.persons[0]:0,self.persons[1]:0,self.persons[2]:0,self.persons[3]:0,self.persons[4]:0}
+        c = ['Food', 'Utilities', 'Travelling', 'Parties', 'Others']
+        self.cwisespends = {"Food": 0, "Utilities": 0, "Travelling": 0, "Parties": 0, "Others": 0}
+        self.spends = {self.persons[0]: 0, self.persons[1]: 0, self.persons[2]: 0, self.persons[3]: 0,
+                       self.persons[4]: 0}
         for i in range(len(self.transactionsar)):
             for j in range(5):
-                if self.transactionsar[i][0]==self.persons[j]:
-                    self.spends[self.transactionsar[i][0]]=self.spends[self.transactionsar[i][0]]+int(self.transactionsar[i][1])
+                if self.transactionsar[i][0] == self.persons[j]:
+                    self.spends[self.transactionsar[i][0]] = self.spends[self.transactionsar[i][0]] + int(
+                        self.transactionsar[i][1])
             for k in c:
-                if self.transactionsar[i][3]==k:
-                    self.cwisespends[k]=self.cwisespends[k]+int(self.transactionsar[i][1])
+                if self.transactionsar[i][3] == k:
+                    self.cwisespends[k] = self.cwisespends[k] + int(self.transactionsar[i][1])
 
         sm.add_widget(Overview(name='overview'))
-        sm.current="overview"
+        sm.current = "overview"
 
         try:
             sm.remove_widget(sm.get_screen('transactions'))
         except kivy.uix.screenmanager.ScreenManagerException:
             pass
-
 
     def transactions(self):
         try:
@@ -920,19 +830,18 @@ class MainApp(MDApp):
             self.category = self.transactionsar[i][3]
 
             self.paymenttime = self.transactionsar[i][4]
-            l=TCard()
+            l = TCard()
             sm.get_screen('transactions').ids.box.add_widget(l)
-    def on_checkbox_active(self,category):
-        self.category=category
 
-    def on_person_select(self,personname):
-        self.personname=personname
+    def on_checkbox_active(self, category):
+        self.category = category
+
+    def on_person_select(self, personname):
+        self.personname = personname
+
     def plus(self, addtrs):
         sm.add_widget(AddTransaction(name='addtransaction'))
         sm.current = 'addtransaction'
 
 
-
-
-MainApp().run()
-
+TripotoApp().run()
